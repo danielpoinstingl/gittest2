3ab91ba44c58a6f770ed5564d44acbfa2e0c95b6
danielpoinstingl <daniel.poinstingl@gmx.at>
2024-04-22 12:03:43 +0200

    add new line in branch daniel

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("newtext");
+        System.out.println("new in branch daniel");
     }
 }
